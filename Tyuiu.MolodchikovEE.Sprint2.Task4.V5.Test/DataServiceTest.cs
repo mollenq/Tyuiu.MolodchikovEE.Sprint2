bd0a89c5c8a2306654e55db965355527deb6cb30
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-09-21 22:37:04 +0500

    Task 4

---
+++ Tyuiu.MolodchikovEE.Sprint2.Task4.V5.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+using Tyuiu.MolodchikovEE.Sprint2.Task4.V5.Lib;
+
+
+namespace Tyuiu.MolodchikovEE.Sprint2.Task4.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.MolodchikovEE.Sprint2.Task4.V5/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.MolodchikovEE.Sprint2.Task4.V5.Lib;
+
+
+
+namespace Tyuiu.MolodchikovEE.Sprint2.Task4.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Молодчиков Е.Е. | ИИПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Молодчиков Егор Евгеньевич | ИИПб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с    *");
+            Console.WriteLine("* использованием вложенных оператор if-else, где пользователь вводит      *");
+            Console.WriteLine("* значение переменной X с клавиатуры. Округлить полученное значение до трех*");
+            Console.WriteLine("* знаков после запятой;                                                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение Х: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение Y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.Calculate(x,y);
+            Console.WriteLine($"Значение Z = {res}");
+
+            Console.ReadLine();
+        }
+    }
+}
