b9488721dad8013838f46ce755b49d85d38c9bff
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-09-21 19:34:16 +0500

    Add project files.

---
+++ Tyuiu.MolodchikovEE.Sprint2.Task0.V1.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+
+
+namespace Tyuiu.MolodchikovEE.Sprint2.Task0.V1.Lib
+{
+    public class DataService : ISprint2Task0V1
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = x - 111 == y;
+            res[1] = x - 111 != y;
+            res[2] = x - 150 < y;
+            res[3] = x - 150 > y;
+            res[4] = x - 150 <= y;
+            res[5] = x - 150 >= y;
+
+            return res;
+        }
+    }
+}
+
+
+
+
+
+// x = 233
+
+// y=  122
+
+
+//  true, fal se, tr ue, false, tr ue, false
---
+++ Tyuiu.MolodchikovEE.Sprint2.Task0.V1/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.MolodchikovEE.Sprint2.Task0.V1.Lib;
+
+namespace Tyuiu.MolodchikovEE.Sprint2.Task0.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Молодчиков Е.Е. | ИИПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Молодчиков Егор Евгеньевич | ИИПб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность операций не должна нарушаться) и арифметических      *");
+            Console.WriteLine("* выражений, которая вернет логическую последовательность(массив):        *");
+            Console.WriteLine("* (True, False, True, False, True, False), при x = 233, y = 122           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("x = 233");
+            int x = 233;
+            Console.WriteLine("y = 122");
+            int y = 122;
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
