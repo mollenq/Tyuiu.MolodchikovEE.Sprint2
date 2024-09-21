f0ec6855c8637ccfd252a838ac1eabf8e0934ef2
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-09-22 02:37:14 +0500

    Task6

--- Tyuiu.MolodchikovEE.Sprint2.Task6.V5.Lib/DataService.cs
+++ Tyuiu.MolodchikovEE.Sprint2.Task6.V5.Lib/DataService.cs
@@ -1,4 +1,4 @@
-п»їusing tyuiu.cources.programming.interfaces.Sprint2;
+using tyuiu.cources.programming.interfaces.Sprint2;
 
 namespace Tyuiu.MolodchikovEE.Sprint2.Task6.V5.Lib
 {
@@ -9,16 +9,16 @@ namespace Tyuiu.MolodchikovEE.Sprint2.Task6.V5.Lib
 
             switch (value)
             {
-                case 6: return "С€РµСЃС‚РµСЂРєР°";
-                case 7: return "СЃРµРјРµСЂРєР°";
-                case 8: return "РІРѕСЃСЊРјРµСЂРєР°";
-                case 9: return "РґРµРІСЏС‚РєР°";
-                case 10: return "РґРµСЃСЏС‚РєР°";
-                case 11: return "РІР°Р»РµС‚";
-                case 12: return "РґР°РјР°";
-                case 13: return "РєРѕСЂРѕР»СЊ";
-                case 14: return "С‚СѓР·";
-                default: throw new ArgumentException("Р§РёСЃР»Рѕ РѕС‚ 6 РґРѕ 14 РґРѕР»Р¶РЅРѕ Р±С‹С‚СЊ!");
+                case 6: return "шестерка";
+                case 7: return "семерка";
+                case 8: return "восьмерка";
+                case 9: return "девятка";
+                case 10: return "десятка";
+                case 11: return "валет";
+                case 12: return "дама";
+                case 13: return "король";
+                case 14: return "туз";
+                default: throw new ArgumentException("Число от 6 до 14 должно быть!");
             }
         }
     }
