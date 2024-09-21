b772b5db8a536bd7c2aa8857918b0124505b8adf
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-09-22 00:14:26 +0500

    Task5

---
+++ Tyuiu.MolodchikovEE.Sprint2.Task5.V8.Lib/DataService.cs
@@ -0,0 +1,56 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.MolodchikovEE.Sprint2.Task5.V8.Lib
+{
+    public class DataService : ISprint2Task5V8
+    {
+        public string FindDateOfPreviousDay(int m, int n)
+        {
+            string res;
+            n -= 1;
+
+            switch (m) 
+            {
+                case 1:
+                    res = "Январь";
+                    break;
+                case 2:
+                    res = "Февраль";
+                    break;
+                case 3:
+                    res = "Март";
+                    break;
+                case 4:
+                    res = "Апрель";
+                    break;
+                case 5:
+                    res = "Май";
+                    break;
+                case 6:
+                    res = "Июнь";
+                    break;
+                case 7:
+                    res = "Июль";
+                    break;
+                case 8:
+                    res = "Август";
+                    break;
+                case 9:
+                    res = "Сентябрь";
+                    break;
+                case 10:
+                    res = "Октябрь";
+                    break;
+                case 11:
+                    res = "Ноябрь";
+                    break;
+                case 12:
+                    res = "Декабрь";
+                    break;
+                default:
+                    throw new ArgumentException($"Должен быть месяц от 1 до 12");
+
+            } return  res + " " + n;
+        } 
+    }
+}
---
+++ Tyuiu.MolodchikovEE.Sprint2.Task5.V8.Test/DataService.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.MolodchikovEE.Sprint2.Task5.V8.Test
+{
+    [TestClass]
+    public class DataService
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
