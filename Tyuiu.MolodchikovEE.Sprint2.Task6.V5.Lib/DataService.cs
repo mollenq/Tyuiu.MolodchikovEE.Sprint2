cba4773afcd2022339768b9a8dc87511f21646ef
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-09-22 02:07:45 +0500

    Task6

--- Tyuiu.MolodchikovEE.Sprint2.Task6.V5.Lib/DataService.cs
+++ Tyuiu.MolodchikovEE.Sprint2.Task6.V5.Lib/DataService.cs
@@ -10,15 +10,15 @@ namespace Tyuiu.MolodchikovEE.Sprint2.Task6.V5.Lib
 
             switch (value) 
             {
-                case 6: return "шестерка";
-                case 7: return "семерка";
-                case 8: return "восьмерка";
-                case 9: return "девятка";
-                case 10: return "десятка";
-                case 11: return "валет";
-                case 12: return "дама";
-                case 13: return "король";
-                case 14: return "туз";
+                case 6: return result = "шестерка";
+                case 7: return result = "семерка";
+                case 8: return result = "восьмерка";
+                case 9: return result = "девятка";
+                case 10: return result = "десятка";
+                case 11: return result = "валет";
+                case 12: return result = "дама";
+                case 13: return result = "король";
+                case 14: return result = "туз";
                 default: throw new ArgumentException("Число должно быть от 6 до 14");
             }
         }
