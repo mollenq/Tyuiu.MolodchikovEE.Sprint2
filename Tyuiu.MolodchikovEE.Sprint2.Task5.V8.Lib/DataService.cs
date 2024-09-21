b788a9fb8754a65288c10cd46a38a898cb53c0d5
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-09-22 00:26:39 +0500

    Task5

--- Tyuiu.MolodchikovEE.Sprint2.Task5.V8.Lib/DataService.cs
+++ Tyuiu.MolodchikovEE.Sprint2.Task5.V8.Lib/DataService.cs
@@ -6,51 +6,11 @@ namespace Tyuiu.MolodchikovEE.Sprint2.Task5.V8.Lib
     {
         public string FindDateOfPreviousDay(int m, int n)
         {
-            string res;
-            n -= 1;
-
-            switch (m) 
-            {
-                case 1:
-                    res = "Январь";
-                    break;
-                case 2:
-                    res = "Февраль";
-                    break;
-                case 3:
-                    res = "Март";
-                    break;
-                case 4:
-                    res = "Апрель";
-                    break;
-                case 5:
-                    res = "Май";
-                    break;
-                case 6:
-                    res = "Июнь";
-                    break;
-                case 7:
-                    res = "Июль";
-                    break;
-                case 8:
-                    res = "Август";
-                    break;
-                case 9:
-                    res = "Сентябрь";
-                    break;
-                case 10:
-                    res = "Октябрь";
-                    break;
-                case 11:
-                    res = "Ноябрь";
-                    break;
-                case 12:
-                    res = "Декабрь";
-                    break;
-                default:
-                    throw new ArgumentException($"Должен быть месяц от 1 до 12");
-
-            } return n + "." + m;
-        } 
+            
+            m -= 1;
+            string result = m+"."+n;
+            return result;
+            
+        }
     }
 }
